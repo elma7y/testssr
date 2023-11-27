4bb869af71b27b7d32675f7f36f280afdef7583b
sameh <elma7y1996@gmail.com>
2023-11-27 18:07:59 +0200

    testing

--- hamdy2/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  BACKEND_DOMAIN: 'https://dashboard-chris.onrender.com',
-  SITE_DOMAIN: 'post-your-biz2.vercel.app',
-};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToastModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService],
 })
 export class AppComponent {
-  title = 'testssr';
+  shouldRender: boolean;
+  isServer = true;
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.shouldRender = false;
+  }
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.isServer = false;
+      });
+    }
+  }
+  ngDoCheck(): void {
+    this.renderCheck();
+  }
+  renderCheck(): void {
+    if (
+      this.router.url !== '/contact-us' &&
+      this.router.url !== '/login' &&
+      this.router.url !== '/reset' &&
+      this.router.url !== '/forget' &&
+      this.router.url !== '/register'
+    ) {
+      this.shouldRender = true;
+    } else {
+      this.shouldRender = false;
+    }
+  }
 }
