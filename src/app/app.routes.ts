606f1f0aea856fa9bafd6c1ef15f543aca75e887
sameh <elma7y1996@gmail.com>
2023-11-27 18:35:24 +0200

    ay hbl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-];
+export const routes: Routes = [];
