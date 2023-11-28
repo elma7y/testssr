fc3f80d78311e50087c8342a1cc25b0facc67253
sameh <elma7y1996@gmail.com>
2023-11-28 15:28:25 +0200

    test yaba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { ByeComponent } from './bye/bye.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: 'bye', component: ByeComponent },
+];
---
+++ src/app/bye/bye.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bye',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bye.component.html',
+  styleUrl: './bye.component.scss'
+})
+export class ByeComponent {
+
+}
